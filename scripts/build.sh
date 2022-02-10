bbb1730b64433909a0755088de8f5a9563757ece
otherchris <chriscaragianis@gmail.com>
2022-02-10 13:47:52 -0500

    thumbs in progress

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,9 +8,9 @@ npm run build
 
 # Copy html
 cp index.html site/index.html
+cp pictures.html site/pictures.html
 cp calc.html site/calc.html
 cp src/calc.js site/scripts/calc.js
 cp picture-list.html site/picture-list.html
 
 # Open site
-open site/index.html
