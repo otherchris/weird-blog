c48d3cd7247f66dae3e239d5ba1136a593930bdb
otherchris <chriscaragianis@gmail.com>
2022-03-12 18:44:27 -0600

    arttrip

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,6 +13,7 @@ cp gallery.html site/gallery.html
 cp calc.html site/calc.html
 cp src/calc.js site/scripts/calc.js
 cp picture-list.html site/picture-list.html
+cp arttrip.html site/arttrip.html
 cp picture_pages/* site/
 
 # Open site
