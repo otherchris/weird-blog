6fc7375d0fcabeb90cc01f264676f61aa4637493
Chris Caragianis <chriscaragianis@Chriss-MacBook-Pro.local>
2023-10-26 19:15:35 -0400

    adjust script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,12 @@
 # Build styles
 sass sass/main.scss site/css/main.css
 
-# Build js files
-npm run build
+# # Build js files
+# npm run build
 
 # Copy html
 cp index.html site/index.html
+cp old_page.html site/old_page.html
 cp pictures.html site/pictures.html
 cp gallery.html site/gallery.html
 cp calc.html site/calc.html
